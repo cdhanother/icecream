8622697187405a24eef64362ae44a5e8d975917c
cdhjane <dongbingnan233@126.com>
2019-04-20 11:17:58 +0800

    Add test

---
+++ test/icecream/AddressTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icecream;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class AddressTest {
+    
+    public AddressTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of format method, of class Address.
+     */
+    @Test
+    public void testFormat() {
+        System.out.println("format");
+        Address instance = null;
+        String expResult = "";
+        String result = instance.format();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
